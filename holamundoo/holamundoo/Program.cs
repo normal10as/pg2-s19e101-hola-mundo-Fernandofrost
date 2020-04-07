25f2a2a86b60b84e6fb3b6e1a81ada32c5bbd7cd
Fernando Correa <slipkkorn@live.com>
2020-04-07 01:13:54 -0300

    Hola mundo c#

---
+++ holamundoo/holamundoo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace holamundoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
